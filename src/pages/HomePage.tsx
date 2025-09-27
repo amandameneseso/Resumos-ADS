db8f74d898d880a18a4e2e1167146eb909128b62
amandameneseso <amandamenezes594@gmail.com>
2025-09-27 13:17:21 -0300

    layout home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -17,9 +17,36 @@ function HomePage() {
       </div>
 
       <h1>Análise e Desenvolvimento de Sistemas</h1>
-      {/* <hr /> */}
-      <p>Bem-vindo(a)!</p>
-      <p>Selecione uma disciplina no menu à esquerda para começar a navegar pelos resumos e materiais.</p>
+      <hr />
+      <div>
+        <p>Bem-vindo(a)! ✧</p>
+        <p> Este é o meu (nosso) cantinho de estudos de Análise e Desenvolvimento de Sistemas! Aqui você encontra conteúdos organizados por disciplina e questões ao final de cada resumo para facilitar seus estudos. Também fiz um calendário para me ajudar na organização das datas das provas e atividades. Seja bem-vindo(a) e aproveite sua estadia! ദ്ദി(˵ •̀ ᴗ - ˵ ).</p>
+        <div className="quote">
+         <p>“O sucesso é a soma de pequenos esforços repetidos dia após dia.”</p>
+         <img className="img-topo" src="images/pen_4.gif" alt="" />
+        </div>
+      </div>
+
+      {/* Seções lado a lado */}
+      <div className="sections-container">
+        <div className="section">
+          <h2><img src="images/newe5.gif" alt="" /> Atualizações</h2>
+          <ul>
+            <p>23-09-2025: Resumos adicionados em Fundamentos de Desenvolvimento de Software.</p>
+            <p>27-09-2025: Atualização no layout da home.</p>
+          </ul>
+        </div>
+
+        <div className="section">
+          <h2>Dicas de estudo</h2>
+          <ul>
+            <li>separe a garrafa de água e beba pelo menos 3x ao dia</li>
+            <li>coloque sua playlist de estudos preferida</li>
+            <li>limpe toda sua mesa/local de estudo</li>
+            <li>Use as questões ao final de cada aula como revisão</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
